f837180eea9fd7228ab4b1ae4e3ce20719ed5ec4
BaeByungjae <39183208+BaeByungjae@users.noreply.github.com>
2021-07-18 01:10:38 +0900

    Delete src directory

--- src/modules/auth.ts
+++
@@ -1,6 +0,0 @@
-const CHANGE_FIELD = 'auth/CHANGE_FIELD' as const;
-const INITIALIZE_FORM = 'auth/INITIALIZE_FORM' as const;
-
-function auth(state: FormState = initialState, action: FormAction) {}
-
-export default auth;

a3cbb635f97f0e8fc5cd08cb50b71f6297d2b6b5
qudwo <qudwo09@naver.com>
2021-08-10 14:40:17 +0900

    file

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,18 +1,30 @@
+/* eslint-disable no-unused-vars */
 import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Post.module.css';
 
 const Post = ({ nickname }) => {
 	const inputRef = useRef();
-	const [imgArr, setImgArr] = useState([]);
+	const [imgBase64, setImgBase64] = useState(''); // 파일 base64
+	const [imgFile, setImgFile] = useState(null);
 	const onClick = (e) => {
 		e.preventDefault();
 		inputRef.current.click();
 	};
 	const onChange = (e) => {
-		console.log(e.currentTarget.files);
-		setImgArr(e.currentTarget.files);
-		console.log(imgArr);
+		const reader = new FileReader();
+
+		reader.onloadend = () => {
+			// 2. 읽기가 완료되면 아래코드가 실행됩니다.
+			const base64 = reader.result;
+			if (base64) {
+				setImgBase64(base64.toString()); // 파일 base64 상태 업데이트
+			}
+		};
+		if (e.target.files[0]) {
+			reader.readAsDataURL(e.target.files[0]); // 1. 파일을 읽어 버퍼에 저장합니다.
+			setImgFile(e.target.files[0]); // 파일 상태 업데이트
+		}
 	};
 	return (
 		<form className={styles.form}>
